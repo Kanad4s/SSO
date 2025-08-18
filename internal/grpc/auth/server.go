3733b72f8d9c200c979f3f03a96679e46627b771
Kanad4s <n.tropin@g.nsu.ru>
2025-08-18 11:56:52 +0700

    add auth constructors

---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,86 @@
+package auth
+
+import (
+	ssov1 "SSO/gen/go/sso"
+	"SSO/internal/grpc/auth"
+	"context"
+	"errors"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+	auth Auth
+}
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appId int,
+	) (token string, err error)
+
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userId int64, err error)
+}
+
+func Register(gRPCServer *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPCServer, &serverAPI{auth: auth})
+}
+
+func (s *serverAPI) Login(
+	ctx context.Context,
+	in *ssov1.LoginRequest,
+) (*ssov1.LoginResponse, error) {
+	if in.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if in.Password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+	if in.GetAppId() == 0 {
+		return nil, status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	// TODO: values validation
+	token, err := s.auth.Login(ctx, in.Email, in.Password, int(in.GetAppId()))
+	if err != nil {
+		if errors.Is(err, auth.ErrInvalidCredentials) {
+			return nil, status.Error(codes.InvalidArgument, "invalid email or password")
+		}
+
+		return nil, status.Error(codes.Internal, "failed to login")
+	}
+
+	return &ssov1.LoginResponse{Token: token}, nil
+}
+
+func (s *serverAPI) Register(
+	ctx context.Context,
+	in *ssov1.RegisterRequest,
+) (*ssov1.RegisterResponse, error) {
+	if in.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if in.Password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	uid, err := s.auth.RegisterNewUser(ctx, in.Email, in.Password)
+	if err != nil {
+		if errors.Is(err, storage.UserExists) {
+			return nil, status.Error(codes.AlreadyExists, "user already exists")
+		}
+
+		return nil, status.Error(codes.Internal, "failed to register user")
+	}
+	return &ssov1.RegisterResponse{UserId: uid}, nil
+}
---
+++ internal/services/auth/auth.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"SSO/internal/domain/models"
+	"context"
+	"log/slog"
+	"time"
+)
+
+// type UserStorage interface {
+// 	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
+// 	User(ctx context.Context, email string) (models.User, error)
+// }
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+type Auth struct {
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
+
+func New(
+	log *slog.Logger,
+	usrSaver UserSaver,
+	usrProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		log:         log,
+		usrSaver:    usrSaver,
+		usrProvider: usrProvider,
+		appProvider: appProvider,
+		tokenTTL:    tokenTTL,
+	}
+}
