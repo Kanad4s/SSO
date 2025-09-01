bd939da0f11e88e92ab91b27d6bcc45564a424a1
Kanad4s <n.tropin@g.nsu.ru>
2025-09-01 11:45:53 +0700

    add interceptors and grpc service run

---
+++ internal/app/grpc/app.go
@@ -0,0 +1,88 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	authgrpc "grpc-service-ref/internal/grpc/auth"
+	"net"
+
+	"log/slog"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int // Порт, на котором будет работать grpc-сервер
+}
+
+func New(log *slog.Logger, authService authgrpc.Auth, port int) *App {
+
+	loggingOpts := []logging.Option{
+		logging.WithLogOnEvents(
+			logging.PayloadSent, logging.PayloadReceived,
+		),
+	}
+
+	recoveryOpts := []recovery.Option{
+		recovery.WithRecoveryHandler(func(p interface{}) (err error) {
+			// TODO
+			// прикрутить интерсепторы для метрики и алерты, и в таком случае в
+			// обработчике паники обязательно нужно отправлять соответствующую метрику
+			// и правильно настроить алерт, это важно.
+			log.Error("Recovery from panic", slog.Any("panic", p))
+
+			// Вернуть честную ошибку или "internal error"
+			return status.Errorf(codes.Internal, "internal error")
+		}),
+	}
+
+	gRPCServer := grpc.NewServer(grpc.ChainUnaryInterceptor(
+		recovery.UnaryServerInterceptor(recoveryOpts...),
+		logging.UnaryServerInterceptor(InterceptorLogger(log), loggingOpts)
+	))
+
+	authgrpc.Register(gRPCServer, authService)
+
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
+
+func InterceptorLogger(l *slog.Logger) logging.Logger {
+	// TODO: hide passwords from request body
+	return logging.LoggerFunc(func(ctx context.Context, lvl logging.Level, msg string, fields ...any) {
+		l.Log(ctx, slog.Level(lvl), msg, fields...)
+	})
+}
+
+
+func (a *App) MustRun() {
+	if err := a.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func (a *App) Run() error {
+	const op = "grpcapp.Run"
+	a.gRPCServer
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	a.log.Info("grpc server started", slog.String("addr", l.Addr().String()))
+
+	if err := a.gRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
